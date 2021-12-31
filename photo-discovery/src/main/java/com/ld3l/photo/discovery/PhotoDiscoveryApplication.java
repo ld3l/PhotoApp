e46a70c42eecab6eff07afe98ae5c47a5bb5e9a5
ld3l <measpnir@gmail.com>
2021-12-31 10:37:31 +0300

    init photo-discovery service

---
+++ photo-discovery/src/main/java/com/ld3l/photo/discovery/PhotoDiscoveryApplication.java
@@ -0,0 +1,13 @@
+package com.ld3l.photo.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoDiscoveryApplication.class, args);
+    }
+
+}
---
+++ photo-discovery/src/test/java/com/ld3l/photo/discovery/PhotoDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ld3l.photo.discovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
