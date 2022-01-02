b1e6d078e3b417918fbe9e56b8c8876496adc582
ld3l <measpnir@gmail.com>
2022-01-03 01:05:01 +0300

    add password encryption

--- photo-user/src/main/java/com/ld3l/photo/user/service/UserServiceImpl.java
+++ photo-user/src/main/java/com/ld3l/photo/user/service/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.ld3l.photo.user.domain.model.CreateUserModel;
 import com.ld3l.photo.user.mapper.UserMapper;
 import com.ld3l.photo.user.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @AllArgsConstructor
@@ -13,11 +14,12 @@ public class UserServiceImpl implements UserService {
 
     private UserMapper userMapper;
     private UserRepository userRepository;
+    private BCryptPasswordEncoder passwordEncoder;
 
     @Override
     public Long createUser(CreateUserModel user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         UserEntity userEntity = userMapper.createUserModelToUserEntity(user);
-
         return userRepository.save(userEntity).getId();
     }
 }
