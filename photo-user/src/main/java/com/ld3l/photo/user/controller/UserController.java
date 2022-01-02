951c3092fe4d4d40ddd381d0d19bc696ddcf7098
ld3l <measpnir@gmail.com>
2022-01-02 16:50:48 +0300

    rename controller package

--- photo-user/src/main/java/com/ld3l/photo/user/controller/UserController.java
+++ photo-user/src/main/java/com/ld3l/photo/user/controller/UserController.java
@@ -1,6 +1,8 @@
-package com.ld3l.photo.user.Controller;
+package com.ld3l.photo.user.controller;
 
 import com.ld3l.photo.user.domain.model.CreateUserModel;
+import com.ld3l.photo.user.service.UserService;
+import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +10,14 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.validation.Valid;
 
 @RestController
+@AllArgsConstructor
 public class UserController {
+
+   private UserService userService;
+
     @PostMapping
     public String createUser(@Valid @RequestBody CreateUserModel model) {
+        userService.createUser(model);
         return model.getFirstName();
     }
 }
