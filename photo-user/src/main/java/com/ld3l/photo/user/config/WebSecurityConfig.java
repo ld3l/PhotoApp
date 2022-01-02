905ace65e7d9232b8f68d59ab5ca6f6de40f5bb9
ld3l <measpnir@gmail.com>
2022-01-03 01:04:50 +0300

    add spring security

---
+++ photo-user/src/main/java/com/ld3l/photo/user/config/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.ld3l.photo.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .authorizeRequests().antMatchers("/users/**").permitAll();
+    }
+
+    @Bean
+    BCryptPasswordEncoder cryptPasswordEncoder(){
+        return new BCryptPasswordEncoder(10);
+    }
+}
--- photo-user/src/main/java/com/ld3l/photo/user/controller/UserController.java
+++ photo-user/src/main/java/com/ld3l/photo/user/controller/UserController.java
@@ -3,21 +3,25 @@ package com.ld3l.photo.user.controller;
 import com.ld3l.photo.user.domain.model.CreateUserModel;
 import com.ld3l.photo.user.service.UserService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
 
-@RestController
+@RestController()
+@RequestMapping("/users")
 @AllArgsConstructor
 public class UserController {
 
    private UserService userService;
 
     @PostMapping
-    public String createUser(@Valid @RequestBody CreateUserModel model) {
-        userService.createUser(model);
-        return model.getFirstName();
+    public ResponseEntity<Long> createUser(@Valid @RequestBody CreateUserModel model) {
+        Long id = userService.createUser(model);
+        return new ResponseEntity<>(id, HttpStatus.CREATED);
     }
 }
