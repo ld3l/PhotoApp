d48a2270e94a4c6fcf8056573dbd1f2f46b4d4a6
ld3l <measpnir@gmail.com>
2022-01-04 13:55:42 +0300

    add getting user by email

--- photo-user/src/main/java/com/ld3l/photo/user/service/UserService.java
+++ photo-user/src/main/java/com/ld3l/photo/user/service/UserService.java
@@ -1,7 +1,9 @@
 package com.ld3l.photo.user.service;
 
+import com.ld3l.photo.user.domain.dto.UserDto;
 import com.ld3l.photo.user.domain.model.CreateUserModel;
 
 public interface UserService {
     Long createUser(CreateUserModel user);
+    UserDto getUserByEmail(String email);
 }
--- photo-user/src/main/java/com/ld3l/photo/user/service/UserServiceImpl.java
+++ photo-user/src/main/java/com/ld3l/photo/user/service/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ld3l.photo.user.service;
 
+import com.ld3l.photo.user.domain.dto.UserDto;
 import com.ld3l.photo.user.domain.entity.UserEntity;
 import com.ld3l.photo.user.domain.model.CreateUserModel;
 import com.ld3l.photo.user.mapper.UserMapper;
@@ -21,5 +22,10 @@ public class UserServiceImpl implements UserService {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         UserEntity userEntity = userMapper.createUserModelToUserEntity(user);
         return userRepository.save(userEntity).getId();
+    }
+
+    @Override
+    public UserDto getUserByEmail(String email) {
+        return userMapper.userEntityToUserDto(userRepository.findByEmail(email));
     }
 }
