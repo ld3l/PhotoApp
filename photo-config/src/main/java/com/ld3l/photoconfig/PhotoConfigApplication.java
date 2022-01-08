976ce95bf40831fd0e40ef9826674d63f57b8ea7
ld3l <measpnir@gmail.com>
2022-01-08 15:32:20 +0300

    ini config server

---
+++ photo-config/src/main/java/com/ld3l/photoconfig/PhotoConfigApplication.java
@@ -0,0 +1,13 @@
+package com.ld3l.photoconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoConfigApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotoConfigApplication.class, args);
+    }
+
+}
